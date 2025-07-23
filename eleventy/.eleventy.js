c6ec4898c1efe343f7f034dd3b3ac81f6a74a60f
Yash Shrestha <yashcrest@gmail.com>
2025-07-23 21:50:02 +1000

    initialize Eleventy project structure and add base template

---
+++ eleventy/.eleventy.js
@@ -0,0 +1,9 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/css");
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
+};
---
+++ eleventy/src/index.md
@@ -0,0 +1,8 @@
+---
+layout: base.njk
+title: Home
+---
+
+# First Blog
+
+Hello world
